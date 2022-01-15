67c357bf4162f40bd15151aa00fdf4af243f947b
corni3011 <wolffcornelius@gmx.de>
2022-01-15 19:51:50 +0100

    Implemented add_news_tipp
    Messages sent now create a database entry for news tipps sent to keep track

--- Update.py
+++ Update.py
@@ -1,6 +1,7 @@
 import sqlite3
 import requests
 from Database import Data
+import time
 
 def get_update(database: Data, api_key: str):
     chats = database.get_chats()
@@ -16,9 +17,15 @@ def get_update(database: Data, api_key: str):
     except Exception as e:
         print(e)
 
-def send_message(message, chat_id, api_key):
+def send_message(message, chat_id, api_key, news_key, type):
+    timestamp = time.time()
+    data = Data()
+    data.add_news_tipp(news_key, type, timestamp)
     requests.get(f'https://api.telegram.org/bot{api_key}/sendMessage?chat_id={chat_id}&text={message}')
 
-def send_message_to_chats(message, chat_ids, api_key):
+def send_message_to_chats(message, chat_ids, api_key, news_key, type):
+    timestamp = time.time()
+    data = Data()
+    data.add_news_tipp(news_key, type, timestamp)
     for chat_id in chat_ids:
         requests.get(f'https://api.telegram.org/bot{api_key}/sendMessage?chat_id={chat_id}&text={message}')
